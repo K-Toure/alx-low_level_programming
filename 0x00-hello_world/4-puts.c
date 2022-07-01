8a5330ef500f37659d550c908d0611381537e231
K-Toure <tourejay@gmail.com>
2022-07-01 06:35:02 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,15 @@
 #!/bin/bash
 gcc puts.c
+#include <stdio>                                                                   
+
+                                                                                   
+
+int main(void)                                                                     
+
+{                                                                                  
+
+puts("\"Programming is like buildinga multiligual puzzle");                        
+
+return (0);                                                                        
+
+}  
---
+++ 0x00-hello_world/test.c
@@ -0,0 +1,7 @@
+#include <stdio>
+
+int main(void)
+{
+puts("\"Programming is like buildinga multiligual puzzle");
+return (0);
+}
