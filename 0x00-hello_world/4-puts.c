135052d60e863654bb93fbce750dd04ca3383c3d
K-Toure <tourejay@gmail.com>
2022-06-30 18:20:41 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,2 @@
-#include <stdio>
-
-int main(void){	
-
-	
-
-puts("\"Programming is like building a multilingual puzzle"); 
-return (0); 
-}
+#!/bin/bash
+#include <stdio.h> int main(void) { puts("Programming is like building a multilingual puzzle\n"); return (0); }
