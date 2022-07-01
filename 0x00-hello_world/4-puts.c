3c0fc35d97793a8c63cc51384101af9c5412bede
K-Toure <tourejay@gmail.com>
2022-07-01 10:43:31 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
- * Description: 'Program C'
- * return: 0
- */
+ * main - main block
+ * Return: 0
+ **/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
