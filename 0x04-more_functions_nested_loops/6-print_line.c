55c7a782b815a1f8430c4712e712b84051ed211c
K-Toure <tourejay@gmail.com>
2022-07-08 04:24:02 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -12,10 +12,9 @@ void more_numbers(void)
 		for (y = 0; x < 15; y++)
 		{
 			if (y >= 10)
-				_putchar((y / 10) + '0');
-
-			_putchar((y % 10) + '0');
+				putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
 		}
-		_putchar('\n');
+		putchar('\n');
 	}
 }
