9c9af4970a8974e7d138c45468dbb2808fd65e79
K-Toure <tourejay@gmail.com>
2022-07-08 04:33:47 -0700

    6-print_line.c

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,20 +1,16 @@
 #include "main.h"
 
 /**
- * more_numbers -> printing more numbers
+ * print_line -> printing line
+ * @n: integer params
  */
-void more_numbers(void)
+void print_line(int n)
 {
-	int x, y;
+	int x;
 
-	for (x = 0; x < 10; x++)
-	{
-		for (y = 0; x < 15; y++)
-		{
-			if (y >= 10)
-				putchar((y / 10) + '0');
-			putchar((y % 10) + '0');
-		}
-		putchar('\n');
-	}
+	if (n <= 0)
+		_putchar('\n');
+	for (x = 0; x <= n; x++)
+		_putchar(95);
+	_putchar('\n');
 }
