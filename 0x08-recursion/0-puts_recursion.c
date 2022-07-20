644b86b3e08eadbd61bf4b079c4600ff38c2877e
K-Toure <tourejay@gmail.com>
2022-07-20 11:50:31 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,5 +1,3 @@
-#include <string.h>
-#include <stdio.h>
 #include "main.h"
 
 /**
@@ -13,7 +11,9 @@ void _puts_recursion(char *s)
 {
 	if (*s == 0)
 	{
+		_putchar("\n")
 		return;
 	}
+	_putchar(*s);
 	_puts_recursion(s + 1);
 }
